1031df8f7c5738ba77a4ee06d05d55ef502abe6e
MDFKBH <teerp7602@gmail.com>
2026-03-11 16:32:27 +0800

    Remove no need files  from repository and add to gitignore

--- test.py
+++
@@ -1,6 +0,0 @@
-import mediapipe
-import os
-print("========================================")
-print("我現在讀到的 MediaPipe 檔案路徑是：")
-print(mediapipe.__file__)
-print("========================================")
